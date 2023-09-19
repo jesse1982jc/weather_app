ae440593e08df81876a02a7c27c3aba80287370f
Jesse <ya710728@gmail.com>
2023-09-19 17:15:17 +0800

    edit some code2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -11,6 +11,10 @@ function App() {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=592de7b2a5c0e9334ea74c503adf5ab9`;
 
+  // useEffect(() => {
+  //   console.log("Rendering...");
+  // }, [data]);
+
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       axios
@@ -21,7 +25,10 @@ function App() {
           console.log(response.data.name);
         })
         .catch((e) => {
-          console.log(e);
+          // setData(e);
+          console.log("ERROR", e);
+          console.log(e.data);
+          console.log(e.name);
         });
 
       setLocation("");
@@ -39,40 +46,46 @@ function App() {
           type="text"
         />
       </div>
+      {data.cod == 200 ? (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <h2>{data.name}</h2>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{Math.floor(data.main.temp)}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+          </div>
 
-      {data.name === undefined && <p className="result">NO RESULT</p>}
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <h2>{data.name}</h2>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{Math.floor(data.main.temp)}°C</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+          {data.name != undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">{Math.floor(data.main.feels_like)}°C</p>
+                ) : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{Math.floor(data.wind.speed)} MPH</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
         </div>
-
-        {data.name != undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold">{Math.floor(data.main.feels_like)}°C</p>
-              ) : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        )}
-      </div>
+      ) : (
+        <p className="result">NO RESULT</p>
+      )}
     </div>
   );
 }
